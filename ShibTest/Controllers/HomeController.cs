5a5384e0a651a99fe6d56fc63c408776dee93f22
yangyer <yangyer81@gmail.com>
2015-04-08 09:22:11 -0700

    Initial checkin of SHIB test application

---
+++ ShibTest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ShibTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShibTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var homeModel = new HomeModel();
+
+            homeModel.ClientUrl = Request.Url.ToString();
+            homeModel.ShibUrl = "https://" + Request.Url.Host + "/Shibboleth.sso";
+            homeModel.EntityId = "https://shibtest.ucmerced.edu/idp/shibboleth";
+
+            return View(homeModel);
+        }
+    }
+}
---
+++ ShibTest/Models/HomeModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShibTest.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HomeModel
+    {
+        /// <summary>
+        /// Gets or sets the shib URL.
+        /// </summary>
+        /// <value>
+        /// The shib URL.
+        /// </value>
+        public string ShibUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the client URL.
+        /// </summary>
+        /// <value>
+        /// The client URL.
+        /// </value>
+        public string ClientUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the entity identifier.
+        /// </summary>
+        /// <value>
+        /// The entity identifier.
+        /// </value>
+        public string EntityId { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeModel"/> class.
+        /// </summary>
+        public HomeModel()
+        {
+
+        }
+    }
+}
